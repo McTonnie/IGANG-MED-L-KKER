05e7d9e26e013b1ad04987a4f7fcb1a7ef97a1ee
Oliver Tobias Jensen <Olivertobias@live.dk>
2021-01-27 12:52:32 +0100

    Add project files.

---
+++ IGANG MED LØKKER/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IGANG_MED_LØKKER
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //#Nemt
+            for (int i = 0; i <= 99; i++)
+            {
+
+
+                if (i < 50)
+                {
+                Console.WriteLine(i);
+
+
+                }
+            }
+            for (int i = 100; i > 0; i--)
+            {
+                    Console.WriteLine(i);
+
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
